7b7b948fe833a7548176fbc0daf698d47b3e8932
Rahul Maurya <rm505125@gmail.com>
2023-01-12 14:11:38 +0530

    connection limits-service to config server, git repo

---
+++ Limit-Services/src/main/java/com/code/microserveces/LimitServices/configuration/CustomConfiguration.java
@@ -0,0 +1,37 @@
+package com.code.microserveces.LimitServices.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/*
+ * Reading value from application.properties
+ * 
+ * */
+
+@Component
+@ConfigurationProperties("limits-service")
+public class CustomConfiguration {
+
+	private int minimum;
+	private int maximum;
+
+	
+	
+	
+	public int getMinimum() {
+		return minimum;
+	}
+
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+
+	public int getMaximum() {
+		return maximum;
+	}
+
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+
+}
--- Limit-Services/src/main/java/com/code/microserveces/LimitServices/controller/LimitsController.java
+++ Limit-Services/src/main/java/com/code/microserveces/LimitServices/controller/LimitsController.java
@@ -1,19 +1,36 @@
 package com.code.microserveces.LimitServices.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.code.microserveces.LimitServices.configuration.CustomConfiguration;
 import com.code.microserveces.LimitServices.entity.Limits;
-
 
 @RestController
 @RequestMapping("/v1")
 public class LimitsController {
 
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	// Dependency injection for configuration class
+	private CustomConfiguration configuration;
+
+	public LimitsController(CustomConfiguration configuration) {
+		super();
+		this.configuration = configuration;
+	}
+
 	@GetMapping("/limits")
 	public Limits retrieveLimits() {
-		return new Limits(1,1000);
+		// return new Limits(1,1000);
+
+		// reading values which are defined in application.properties with the
+		// help of Configuration class.
+		logger.info("Minimum: "+configuration.getMinimum()+" Maximum: "+configuration.getMaximum());
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
 	}
-	
+
 }
